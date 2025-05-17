b95dea6ef71f04ad31b0935411b8a3e008027a7b
majdyaq <majd@feelit.tech>
2025-05-17 11:10:46 +0300

    using db in admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,51 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { db } from "@/db/db";
 import React from "react";
 
-function AdminDashboard() {
-  return <div>AdminDashboard</div>;
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricePaidInCents: true },
+    _count: true,
+  });
+
+  return {
+    amount: (data._sum.pricePaidInCents || 0) / 100,
+    numerOfSales: data._count,
+  };
+}
+export default async function AdminDashboard() {
+  const { amount, numerOfSales } = await getSalesData();
+  return (
+    <div className="grin grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard
+        title="Sales"
+        subtitle={`${numerOfSales}`}
+        body={`${amount}`}
+      />
+    </div>
+  );
 }
 
-export default AdminDashboard;
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>{body}</CardContent>
+    </Card>
+  );
+}
