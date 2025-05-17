aa7a1e9cf1ccaae1b1eb8ff7420cf77d6747e18b
majdyaq <majd@feelit.tech>
2025-05-17 10:05:19 +0300

    navbar setup

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Nav, NavLink } from "@/components/Nav";
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Nav>
+        <NavLink href="/">Dashboard</NavLink>
+        <NavLink href="/admin/products">Products</NavLink>
+        <NavLink href="/users">Customers</NavLink>
+        <NavLink href="/orders">Sales</NavLink>
+      </Nav>
+      <div className="container my-6"></div>
+    </>
+  );
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ComponentProps, ReactNode, use } from "react";
+
+export function Nav({ children }: { children: ReactNode }) {
+  return (
+    <nav className="bg-primary text-primary-foreground flex justify-center px-4">
+      {children}
+    </nav>
+  );
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, "className">) {
+  const pathmame = usePathname();
+  return (
+    <Link
+      {...props}
+      className={cn(
+        `p-4 
+        hover:bg-secondary hover:text-secondary-foreground
+        foucs:bg-secondary foucs:text-secondary-foreground`,
+        pathmame === props.href ? "bg-backgroud text-foreground" : ""
+      )}
+    />
+  );
+}
